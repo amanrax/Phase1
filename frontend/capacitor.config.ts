c195006d9c139e8ae1def2f8d315b63f7fe4bca3
Cem2025 <cem2602025@gmail.com>
2025-12-30 14:40:54 +0000

    logo added

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -7,30 +7,39 @@ const config: CapacitorConfig = {
   appId: 'com.cem.farmerapp',
   appName: 'CEM Farmer',
   webDir: 'dist',
+  
   server: {
     androidScheme: 'https',
-    cleartext: true
+    cleartext: true,
+    hostname: 'localhost'
   },
+  
   android: {
     allowMixedContent: true,
     captureInput: true,
     webContentsDebuggingEnabled: true,
     loggingBehavior: 'debug'
   },
+  
   ios: {
     contentInset: 'automatic',
+    scrollEnabled: true,
     allowsBackForwardNavigationGestures: false
   },
+  
   plugins: {
     SplashScreen: {
-      launchShowDuration: 1500,
-      backgroundColor: '#4f46e5',
-      showSpinner: false,
-      androidSpinnerStyle: 'small',
-      iosSpinnerStyle: 'small'
+      launchShowDuration: 2000,
+      backgroundColor: '#1E3A5F',
+      showSpinner: true,
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'large',
+      spinnerColor: '#4ADE80',
+      splashFullScreen: true,
+      splashImmersive: true,
+      androidScaleType: 'CENTER_CROP'
     }
   }
 };
 
 export default config;
-
